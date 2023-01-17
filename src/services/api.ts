070997a306220ccbe4ae390378bd1594809a8d90
Victor Bruno <victorbrunof@icloud.com>
2023-01-17 19:27:26 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+// export const api = axios.create({
+//   baseURL: 'http://localhost:3000/api',
+// });
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL:
+    'https://prj-dashboard-nextjs-3ggx-542ab3lfr-victorbrunof.vercel.app/api',
 });
